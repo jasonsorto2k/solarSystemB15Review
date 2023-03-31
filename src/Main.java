79f0260e6bc37a5a4aea4718cd08cae3937d7aa4
jasonsorto2k <jasonsorto2k@gmail.com>
2023-03-31 08:42:35 -0400

    Issues in sun still resolving

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,10 @@ public class Main {
         solar.planets = "2";
         solar.moon = "2";
         solar.sun = "1";
+
+        sun fero = new sun();
+        fero.color="Red";
+        fero.temperature=34234;
+        fero.heat="3243243243k";
     }
 }
